34190e4f03d517d58647bcca7873e94af09d7e42
DESKTOP-RBGTKHC\Sido_ <Sido_denis@mail.ru>
2021-11-04 15:18:01 +0300

    task 3 fix

--- src/aura/Task3Component/Task3ComponentController.js
+++ src/aura/Task3Component/Task3ComponentController.js
@@ -1,18 +1,21 @@
-/**
- * Created by Sido_ on 17.10.2021.
- */
-
 ({
-    doInit: function(component, event, helper) {
-        helper.fetchAccountTypePicklist(component);
-        helper.fetchCargoTypePicklist(component);
-        helper.fetchCitiesPicklist(component);
-    },
-    createRequest: function (component, event, helper){
-        let allValid = helper.checkValidity(component);
-        if(allValid){
+    createRequest: function (component, event, helper) {
+        let companyValid = component.get("v.CompanyValid");
+        let contactValid = component.get("v.ContactValid");
+        let otherValid = component.get("v.OtherValid");
+        if (companyValid && contactValid && otherValid) {
             helper.launchCreateRequest(component);
             helper.clearFields(component);
+            helper.fireToast(component);
         }
     },
+    handleCompanyInfoEvent: function (component, event, helper) {
+        helper.setCompanyInfo(component, event);
+    },
+    handleContactInfoEvent: function (component, event, helper) {
+        helper.setContactInfo(component, event);
+    },
+    handleOtherInfoEvent: function (component, event, helper) {
+        helper.setOtherInfo(component, event);
+    }
 });
